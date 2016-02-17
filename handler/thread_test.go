8f586a06252d6941bf27bd32b498762e23fac647
ito_shohei <ito_shohei@cyberagent.co.jp>
2016-02-17 13:54:38 +0900

    add test

--- handler/thread_test.go
+++ handler/thread_test.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"github.com/golang/mock/gomock"
+	"github.com/shohhei1126/bbs-go/dao"
 	"github.com/shohhei1126/bbs-go/http/response"
+	"github.com/shohhei1126/bbs-go/model"
 	"github.com/shohhei1126/bbs-go/service"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
@@ -16,6 +18,11 @@ func TestThreadList(t *testing.T) {
 	defer ctl.Finish()
 
 	threadServiceMock := service.NewMockThread(ctl)
+	threadServiceMock.
+		EXPECT().
+		FindThreads(dao.Paging{Limit: 5, Offset: 0, OrderBy: "updated_at desc"}).
+		Return(model.ThreadSlice{}, nil).
+		Times(1)
 	threadHandler := NewThread(threadServiceMock)
 
 	ctx := context.Background()
@@ -28,5 +35,15 @@ func TestThreadList(t *testing.T) {
 		r.URL = url
 		res := threadHandler.List(ctx, &r)
 		assert.Equal(t, response.BadRequest, res, "")
+	}
+
+	{
+		r := http.Request{}
+		url, err := url.Parse("http://localhost?limit=5&offset=0")
+		if err != nil {
+			t.Fatal(err)
+		}
+		r.URL = url
+		threadHandler.List(ctx, &r)
 	}
 }
